7bda88caee29520725ee1b41a42cd00d2449f05e
alex <alex wsl>
2025-02-19 02:29:07 -0300

    fix: astro depencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://latticecorp.cloud/',
+  site: 'http://latticecorp.cloud/',
   integrations: [tailwind()],
   adapter: cloudflare({
     imageService: 'passthrough'
