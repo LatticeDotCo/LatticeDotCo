06b2517c7bffade6ce93dc798b97dcb6a6863c29
alex <alex wsl>
2025-02-19 00:23:35 -0300

    feat: fix template config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
-
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel';
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://positivustheme.vercel.app",
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
-});
+  site: 'https://lattice.cloud',
+})
