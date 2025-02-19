c4df796e8cb4f214fcc4afbbe6c60f084d119c5c
alex <alex wsl>
2025-02-19 02:11:23 -0300

    fix: astro.config.mjs file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://latticecorp.cloud/'
+  site: 'https://latticecorp.cloud/',
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: cloudflare(),
 })
